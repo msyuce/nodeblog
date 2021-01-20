21fc8ebc2339593b9872d9eb3fba9b3124fa0fc5
msyuce <msyuce@hotmail.com>
2021-01-20 18:34:55 +0200

    25 - Kategori Ekleme - HTML

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -3,7 +3,12 @@ const router = express.Router()
 
 
 router.get('/', (req, res) => {
-    res.redirect('/site/admin')
+    res.render('admin/index')
 })
+
+router.get('/categories', (req, res) => {
+    res.render('admin/categories')
+})
+
 
 module.exports = router
--- routes/main.js
+++ routes/main.js
@@ -8,12 +8,11 @@ router.get('/', (req, res) => {
     res.render('site/index')
 })
 
-router.get('/admin', (req, res) => {
-    res.render('admin/index')
-})
+// router.get('/admin', (req, res) => {
+//     res.render('admin/index')
+// })
 
 router.get('/blog', (req, res) => {
-
     Post.find({}).then(posts => {
         res.render('site/blog', { posts: posts })
     })
